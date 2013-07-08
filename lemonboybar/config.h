98b8f84821b09ebb139eb1e3aefeef4cbb93f347
kopri-nb <nsbontehinternet@gmail.com>
2013-07-08 09:35:24 -0700

    bar config.h

---
+++ lemonboybar/config.h
@@ -0,0 +1,28 @@
+/* The height of the bar (in pixels) */
+#define BAR_HEIGHT  14
+/* The width of the bar. Set to -1 to fit screen */
+#define BAR_WIDTH   -1
+/* Offset from the left. Set to 0 to have no effect */
+#define BAR_OFFSET 0
+/* Choose between an underline or an overline */
+#define BAR_UNDERLINE 1
+/* The thickness of the underline (in pixels). Set to 0 to disable. */
+#define BAR_UNDERLINE_HEIGHT 2
+/* Default bar position, overwritten by '-b' switch */
+#define BAR_BOTTOM 0
+/* The fonts used for the bar, comma separated. Only the first 2 will be used. */
+#define BAR_FONT       "-benis-uushi-medium-r-normal--11-90-75-75-p-58-iso10646-1","fixed"
+/* Some fonts don't set the right width for some chars, pheex it */
+#define BAR_FONT_FALLBACK_WIDTH 6
+/* Color palette */
+
+#define COLOR0	0x151515
+#define COLOR1	0xebebeb
+#define COLOR2	0x393939
+#define COLOR3	0xBF1E2D
+#define COLOR4	0x9DBA3A
+#define COLOR5	0xd0d26b
+#define COLOR6	0x6d9cbe
+#define COLOR7	0xB7416E
+#define COLOR8	0x435d75
+#define COLOR9	0xebebeb
