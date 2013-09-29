74cf6fed6925ea29888b0fe0901a3eb5cee73f4b
kopri-nb <kopri@unixhub.net>
2013-09-29 11:22:59 -0600

    updated ZSH for tbuck <3

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,24 +1,3 @@
-local IT="${terminfo[sitm]}${terminfo[bold]}"
-local ST="${terminfo[sgr0]}${terminfo[ritm]}"
-
-local FMT_BRANCH="%F{9}(%s:%F{7}%{$IT%}%r%{$ST%}%F{9}) %F{11}%B%b %K{235}%{$IT%}%u%c%{$ST%}%k"
-local FMT_ACTION="(%F{3}%a%f)"
-local FMT_PATH="%F{1}%R%F{2}/%S%f"
-
-setprompt() {
-  local USER="%(#.%F{1}.%F{2})%n%f"
-  local HOST="%F{1}%M%f"
-  local PWD="%F{7}$($XDG_CONFIG_HOME/zsh/rzsh_path)%f"
-  local TTY="%F{5}%y%f"
-  local EXIT="%(?..%F{202}%?%f)"
-  local PRMPT="${USER}@$HOST:${TTY}: ${PWD}
-${EXIT} %F{202}»%f "
-
-  if [[ "${vcs_info_msg_0_}" == "" ]]; then
-    PROMPT="$PRMPT"
-  else
-    PROMPT="${vcs_info_msg_0_}
-$PRMPT"
-  fi
-}
-
+#PROMPT='%B%F{white}[%B%F{black}%~%B%F{white}]%B%F{white}--%B%F{red}» %f'
+PROMPT=' %B%F{red}» %f'
+RPROMPT='%B%F{black}%~ %B%F{white}%#'
