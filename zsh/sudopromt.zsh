612cc3a07869b62a145c477f3d59acbb768359f1
kopri-nb <kopri@unixhub.net>
2013-10-02 12:37:13 -0700

    sudo prompt

--- zsh/sudopromt.zsh
+++ zsh/sudopromt.zsh
@@ -1,2 +1,5 @@
-#Sudo password prompt
-export SUDO_PROMT="[sudo] password for %u:"
+if type sudo >/dev/null 2>/dev/null; then
+
+   #sudo prompt should mention the user name
+   alias sudo='sudo -p "[sudo] enter password for %u: " '
+  fi
